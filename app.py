c5a28ef76eb1d66fa3cf9834c9a4345de7ce0196
WMoorby <WMoorby@spartaglobal.com>
2021-08-23 15:30:59 +0100

    Adding Registers files, changing txt to json, relvant updates to make that work

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
 from airport_booking_app.app_classes import Passenger, FlightTrip, Plane
+import json
+
+passenger_dict = {}
+flight_trip_dict = {}
+planes_dict = {}
 
 app = Flask(__name__)
+
+# # Change paths for your directories!!
+# parent_dir = os.path.dirname(__file__)
+# airport_booking_app_dir = os.path.dirname(parent_dir)
 
 
 @app.route('/')
@@ -11,9 +20,10 @@ def index():
 
 @app.route('/passenger')
 def passenger():
-    with open("passenger_register.txt", "r") as file:
-        content = file.read()
-    return render_template('passenger/passenger.html', content=content)
+    with open("airport_booking_app\\Registers\\passenger_register.json", "r") as file:
+        content = json.load(file)
+        register = content["passenger_register"]
+    return render_template('passenger/passenger.html', content=register)
 
 
 @app.route('/passenger/add', methods=['POST', 'GET'])
@@ -23,15 +33,12 @@ def addpassenger():
     elif request.method == 'POST':
         name = request.form["name"]
         passport_num = request.form["passportnum"]
-        new_user = Passenger(name, passport_num)
+        passenger_id = "p" + passport_num
+        passenger_dict[passenger_id] = Passenger(name, passport_num)
         # VERIFY
-        if new_user.is_passport_valid():
-            # If it passes, carry on
-            string_pas_num = "p" + str(new_user.passport_num)
-            globals()[string_pas_num] = Passenger(name, passport_num)
-            with open("passenger_register.txt", "a+") as register:
-                register.write(string_pas_num + ", " + name + ", " + passport_num + "\n")
-            return f"Passenger {globals()[string_pas_num].name} has been created with user name {string_pas_num}. <p><a href='/passenger/add'>Back</a></p>"
+        if passenger_dict[passenger_id].is_passport_valid():
+            passenger_dict[passenger_id].write_to_register()
+            return f"Passenger {passenger_dict[passenger_id].user_info['name']} has been created with passport number {passenger_dict[passenger_id].user_info['passport_number']}. <p><a href='/passenger/add'>Back</a></p>"
         # else return error message
         else:
             return f"Invalid passport number {passport_num}. Please use 9 integers. <p><a href='/passenger/add'>Back</a></p>"
@@ -54,10 +61,10 @@ def addflighttrip():
     elif request.method == 'POST':
         destination = request.form["destination"]
 
-        globals()[destination] = FlightTrip(destination)
-        with open("destinations_register.txt", "a+") as register:
-            register.write(destination + "\n")
-        return f"Flight trip {globals()[destination].destination} has been created. <p><a href='/flighttrip/add'>Back</a></p>"
+    flight_trip_dict[destination] = FlightTrip(destination)
+    flight_trip_dict[destination].write_to_register()
+    return f"Flight trip {destination} has been created. <p><a href='/flighttrip/add'>Back</a></p>"
+
 
 
 @app.route('/flighttrip/addpassengertoflight', methods=['POST', 'GET'])
@@ -66,9 +73,9 @@ def addpassengertoflight():
         return render_template('flighttrip/addpassengertoflight.html')
     elif request.method == 'POST':
         flighttrip = request.form["flighttrip"]
-        passengerid = request.form["passengerid"]
-        globals()[flighttrip].add_Passenger(globals()[passengerid])
-        return f"Passenger {passengerid} has been added to flight {flighttrip}. <p><a href='/flighttrip/addpassengertoflight'>Back</a></p>"
+        passenger_id = request.form["passenger_id"] # change HTML
+        flight_trip_dict[flighttrip].add_Passenger(passenger_dict[passenger_id])
+        return f"Added {passenger_id} to {flighttrip}. <p><a href='/flighttrip/'>Back</a></p>"
 
 
 @app.route('/flighttrip/assignplane', methods=['POST', 'GET'])
@@ -78,8 +85,7 @@ def assignplane():
     elif request.method == 'POST':
         planeid = request.form["planeid"]
         flighttrip = request.form["flighttrip"]
-
-        globals()[flighttrip].assign_plane(globals()[planeid])
+        flight_trip_dict[flighttrip].assign_plane(planes_dict[planeid])
         return f"Plane {planeid} has been added to flight {flighttrip}. <p><a href='/flighttrip/assignplane'>Back</a></p>"
 
 
@@ -117,11 +123,9 @@ def addplane():
     elif request.method == 'POST':
         name = request.form["planeid"]
         capacity = request.form["capacity"]
-
-        globals()[name] = Plane(capacity)
-        with open("plane_register.txt", "a+") as register:
-            register.write(name + ', ' + capacity + "\n")
-        return f"Plane {name} has been created. <p><a href='/plane/add'>Back</a></p>"
+        planes_dict[name] = Plane(name, capacity)
+        planes_dict[name].write_to_register()
+    return f"Plane {name} has been created. <p><a href='/plane/add'>Back</a></p>"
 
 
 
